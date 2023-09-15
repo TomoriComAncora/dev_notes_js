a79a9f35eceb4d9f697ebf10d41c5037fc16f06f
TomoriComAncora <lucas_fernando@ufms.br>
2023-09-15 15:46:41 -0400

    termino_expediente

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-console.log("teste");
+const addNotesContainer = document.querySelector("#add-notes-container");
+const addNotesInput = document.querySelector("#add-notes-input");
+const addNote = document.querySelector("add-note");
